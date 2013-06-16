2921da06ca76b214c337c18f82f0860ad34d86a3
Jean-Baptiste Queru <jbq@google.com>
2013-06-16 10:16:02 -0700

    Update for JWR66N
    Change-Id: I1583e7cba6c21678c67cf855c81dbc613ab10b9c

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -51,6 +51,7 @@
 # 699533 = JWR66
 # 701448 = JWR66C
 # 704243 = JWR66G
+# 711294 = JWR66N
 # end jb-mr2-dev
 BRANCH=jb-mr2-dev
 if test $BRANCH = jb-dev
@@ -70,8 +71,8 @@ then
 fi # jb-mr1.1-dev
 if test $BRANCH = jb-mr2-dev
 then
-  ZIP=nakasi-ota-704243
-  BUILD=jwr66g
+  ZIP=nakasi-ota-711294
+  BUILD=jwr66n
 fi # jb-mr2-dev
 ROOTDEVICE=grouper
 DEVICE=grouper
