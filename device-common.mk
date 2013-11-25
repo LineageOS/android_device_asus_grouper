880f6f805c2273bc9c208a74d5ee6fba8a3d9ca5
Ying Wang <wangying@google.com>
2013-11-25 13:48:48 -0800

    Push wpa_supplicant modules down to the leaf product config.
    Since 558477c0b2a5a7db679c1ad7551d43e6364d1df2 in
    external/wpa_supplicant_8 we don't need those modules if
    WPA_SUPPLICANT_VERSION is not set by the BoardConfig.mk.
    
    Change-Id: Idca392a07689ed398b9f876d31212d042801a8ed

--- device-common.mk
+++ device-common.mk
@@ -67,6 +67,12 @@ PRODUCT_COPY_FILES += \
     device/asus/grouper/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
 PRODUCT_PACKAGES := \
+    libwpa_client \
+    hostapd \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
     lights.grouper \
     audio.primary.grouper \
     power.grouper \
