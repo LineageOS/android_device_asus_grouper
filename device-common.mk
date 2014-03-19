fd8ee42ff7c93f6e35775bfcbd4afacaedee5b89
Lajos Molnar <lajos@google.com>
2014-03-19 14:28:37 -0700

    stagefright: use media_codecs_google*.xml files
    Bug: 13549114
    Change-Id: I247b92e4be487a2d838a8689dbd3d04bd9d43a51

--- device-common.mk
+++ device-common.mk
@@ -111,6 +111,9 @@ PRODUCT_COPY_FILES += \
 
 # media codec config xml file
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/grouper/media_codecs.xml:system/etc/media_codecs.xml
 
 # audio mixer paths
