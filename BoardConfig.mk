e8ec17981cbddcc60ceeaa7e6cf25d3da33c3a72
Doug Zongker <dougz@android.com>
2014-04-01 12:50:46 -0700

    remove custom recovery UI for grouper
    All its functionality is now in the default UI.
    
    Change-Id: I0a85bfb4f72435522705ba5d51ec6a176a9d6cfa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,8 +33,6 @@ USE_PROPRIETARY_AUDIO_EXTENSIONS := false
 
 BOARD_HAL_STATIC_LIBRARIES := libdumpstate.grouper
 
-TARGET_RECOVERY_UI_LIB := librecovery_ui_grouper
-
 TARGET_RELEASETOOLS_EXTENSIONS := device/asus/grouper
 
 -include vendor/asus/grouper/BoardConfigVendor.mk
