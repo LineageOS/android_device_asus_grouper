4b937084d087c5c780361a7300143d6956030301
Stephen Smalley <sds@tycho.nsa.gov>
2015-04-01 10:30:08 -0400

    Drop BOARD_SEPOLICY_UNION.
    As suggested in the comments on
    https://android-review.googlesource.com/#/c/141560/
    drop BOARD_SEPOLICY_UNION and simplify the build_policy logic.
    Union all files found under BOARD_SEPOLICY_DIRS.
    
    Change-Id: I4214893c999c23631f5456cb1b8edd59771ef13b
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,28 +82,4 @@ BOARD_USES_GROUPER_MODULES := true
 
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
-BOARD_SEPOLICY_DIRS += \
-        device/asus/grouper/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-        file_contexts \
-        genfs_contexts \
-        bluetooth.te \
-        device.te \
-        domain.te \
-        drmserver.te \
-        init_shell.te \
-        file.te \
-        gpsd.te \
-        keystore.te \
-        lmkd.te \
-        mediaserver.te \
-        recovery.te \
-        rild.te \
-        sensors_config.te \
-        surfaceflinger.te \
-        system_app.te \
-        system_server.te \
-        ueventd.te \
-        vold.te \
-        radio.te
+BOARD_SEPOLICY_DIRS += device/asus/grouper/sepolicy
