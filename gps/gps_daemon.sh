27981ba5ead9d79516a0dc219da227aa195d0617
Ziyan <jaraidaniel@gmail.com>
2016-08-30 20:48:28 +0200

    grouper: move gps.xml to the device tree (2/2)

--- gps/gps_daemon.sh
+++ gps/gps_daemon.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 # shellscript as this is the only way selinux will allow this to proceed
-/vendor/bin/glgps -c /system/etc/gps/gpsconfig.xml
+/vendor/bin/glgps -c /system/etc/gps.xml
