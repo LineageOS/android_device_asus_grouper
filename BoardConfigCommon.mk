d126b0d8890cee67128b3add1ce38cce31fb5286
André Pinela <sheffzor@gmail.com>
2016-09-03 04:59:09 +0100

    grouper: configure jemalloc for low-memory
    https://github.com/android/platform_build/blob/nougat-release/core/envsetup.mk#L162
    
    Change-Id: I42cd3a74d36388899a2be6f998853c4a1f1cb2cf
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,9 @@ BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
 # Only pre-optimize the boot image
 WITH_DEXPREOPT_BOOT_IMG_ONLY := true
 
+# Configure jemalloc for low-memory
+MALLOC_SVELTE := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME)
 TARGET_KERNEL_CONFIG := grouper_defconfig
