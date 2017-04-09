de6765ecd96b493c7c57644ba52c5d2af6c7310c
Harry Youd <harry@harryyoud.co.uk>
2017-04-09 15:28:58 +0100

    grouper: Add lineage makefiles and dependencies
    Change-Id: Iaa65251574f65f83346b4b9da616863f48d73924

---
+++ lineage.mk
@@ -0,0 +1,23 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus7
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/grouper/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := grouper
+PRODUCT_NAME := lineage_grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
+
